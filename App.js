4e61528268697a9aadb2eadd3dfae1d50e6bd4d1
Dates <51355892+hannakj@users.noreply.github.com>
2024-08-25 14:26:13 +0300

    Tehtavan palautus

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [firstNum, setFirstNum] = useState("");
+  const [secondNum, setSecondNum] = useState("");
+  const [result, setResult] = useState(0);
+
+  const addNum = () => {
+    const addedNum = parseInt(firstNum) + parseInt(secondNum);
+    setResult(addedNum);
+    setFirstNum("");
+    setSecondNum("");
+  };
+
+  const subtractNum = () => {
+    const subtractedNum = parseInt(firstNum) - parseInt(secondNum);
+    setResult(subtractedNum);
+    setFirstNum("");
+    setSecondNum("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+          Result: {result}
+        </Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        onChangeText={(firstNum) => setFirstNum(firstNum)}
+        value={firstNum}
+        keyboardType="numeric"
+      ></TextInput>
+      <TextInput
+        style={styles.input}
+        onChangeText={(secondNum) => setSecondNum(secondNum)}
+        value={secondNum}
+        keyboardType="numeric"
+      ></TextInput>
+      <View style={{ flexDirection: "row" }}>
+        <Button onPress={addNum} title="+" />
+        <Button onPress={subtractNum} title="-" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +52,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderWidth: 1,
+    padding: 10,
   },
 });
